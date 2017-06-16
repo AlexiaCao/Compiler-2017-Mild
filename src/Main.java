b94d4081c875cda57e3d1c03ea3aa825e6b6dde4
AlexiaCao <425957001@qq.com>
2017-06-16 10:51:38 +0800

    version Environment fixed+

--- src/Main.java
+++ src/Main.java
@@ -27,10 +27,10 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) throws Exception {
         Utility.arguments = new HashSet<>(Arrays.asList(args));
-        //InputStream is = new FileInputStream("program.txt");
+        InputStream is = new FileInputStream("program.txt");
         try {
-            new Main().compile(System.in, System.out);
-            //new Main().compile(is, System.out);
+            //new Main().compile(System.in, System.out);
+            new Main().compile(is, System.out);
         } catch (CompilationError e) {
             System.err.println(e.getMessage());
             System.exit(1);
